224485f906a1e8d0a5ed6bb5307c318c557eb836
Radif <23radif@gmail.com>
2020-02-23 01:35:43 +0300

    complex nums

--- lesson08/task_7.py
+++ lesson08/task_7.py
@@ -9,12 +9,18 @@
 
 
 class ComplexNumber:
+    def __init__(self, num_1, num_2=0):
+        self.num_1, self.num_2 = num_1, num_2
+
     def __add__(self, other):
-        pass
+        return complex(self.num_1, self.num_2) + complex(other.num_1, other.num_2)
 
     def __mul__(self, other):
-        pass
+        return complex(self.num_1, self.num_2) * complex(other.num_1, other.num_2)
 
 
-complex_1 = ComplexNumber()
-complex_2 = ComplexNumber()
+complex_1 = ComplexNumber(22, 2)
+complex_2 = ComplexNumber(23, 2)
+
+print(complex_1 + complex_2)
+print(complex_1 * complex_2)
